a85a3806a8294b578dfa3b26b1b10c48dce032fd
RAY-2 <ray04041996@gmail.com>
2021-05-08 21:07:40 +0530

    Added Tabbed Layouts for Discover and Bookmark

--- app/src/main/java/com/ray_apps/imageexplorer/MainActivity.java
+++ app/src/main/java/com/ray_apps/imageexplorer/MainActivity.java
@@ -1,14 +1,53 @@
 package com.ray_apps.imageexplorer;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
+import android.graphics.Color;
+import android.graphics.ColorFilter;
 import android.os.Bundle;
+import android.os.Message;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPagerAdaptor viewPagerAdaptor;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle(getString(R.string.app_name));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.actionbar);
+
+        //Tabbed Activity
+
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.viewPager);
+        viewPagerAdaptor = new ViewPagerAdaptor(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+
+        //viewPagerAdaptor
+
+        viewPagerAdaptor.AddFragment(new Discover(),"Discover");
+        viewPagerAdaptor.AddFragment(new Bookmark(),"Bookmarks");
+
+        viewPager.setAdapter(viewPagerAdaptor);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.getTabAt(0).setIcon(R.drawable.discover);
+        tabLayout.getTabAt(1).setIcon(R.drawable.bookmark);
     }
+
 }
---
+++ app/src/main/java/com/ray_apps/imageexplorer/ViewPagerAdaptor.java
@@ -0,0 +1,48 @@
+package com.ray_apps.imageexplorer;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewPagerAdaptor extends FragmentPagerAdapter {
+
+    private final List<Fragment> listFragment = new ArrayList<>();
+    private final List<String> listTitle = new ArrayList<>();
+
+    //constructor
+
+
+    public ViewPagerAdaptor(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return listFragment.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return listTitle.size();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return super.getPageTitle(position);
+
+    }
+
+    //Add fragment
+    public void AddFragment(Fragment fragment, String title)
+    {
+        listFragment.add(fragment);
+        listTitle.add(title);
+    }
+}
